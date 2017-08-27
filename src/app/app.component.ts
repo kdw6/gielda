4dc416ad697eba9d4a98781b75907373401fd2c3
kdw6 <31041680+kdw6@users.noreply.github.com>
2017-08-27 11:06:41 +0200

    zmiana ceny

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,11 +29,20 @@ export class Transakcja{
   prowizja:number;
 }
 const SPOLKI: Spolka[]=[
-{id:1,name:'CCC', cena:149},
-{id:2,name:'ETF 20',cena:288},
-{id:3,name:'Comarch',cena:195}
+{id:1,name:'Centrum Klima', cena:149},
+{id:2,name:'Boryszew',cena:288},
+{id:3,name:'Lotos',cena:196},
+{id:4,name:'Etf Wig 20',cena:195},
+{id:5,name:'Comarch',cena:100},
+{id:6,name:'CCC',cena:195},
+{id:7,name:'Live Chat',cena:195},
+{id:8,name:'Ursus',cena:195}
 ]
 const TRANSAKCJE:Transakcja[]=[
-{id:1,idSpolka:1,data:'2017-05-05',cenaZak:23,ilosc:12,prowizja:3.12}
+{id:1,idSpolka:1,data:'2012-03-06',cenaZak:11.49,ilosc:86,prowizja:3.85},
+{id:2,idSpolka:2,data:'2012-04-04',cenaZak:0.72,ilosc:1364,prowizja:3.83},
+{id:3,idSpolka:1,data:'2012-04-04',cenaZak:11.4,ilosc:87,prowizja:3.87},
+{id:4,idSpolka:2,data:'2012-04-20',cenaZak:0.75,ilosc:1992,prowizja:5.83}
+
 
 ]
